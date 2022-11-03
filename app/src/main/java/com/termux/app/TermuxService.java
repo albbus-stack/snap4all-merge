2afa055e07d1a2770f9e02bd948edfd9dca76ac6
federicomarra <federico.marra@stud.unifi.it>
2022-11-03 15:35:26 +0100

    pkg-update-flag-fixed

--- app/src/main/java/com/termux/app/TermuxService.java
+++ app/src/main/java/com/termux/app/TermuxService.java
@@ -169,6 +169,7 @@ public final class TermuxService extends Service implements AppShell.AppShellCli
                 "chmod +x "+PREFIX_PATH+"/bin/npm\n"+
                 // Installs a custom termux-api package
                 "[ -d "+apiPackagePath+" ] || pkg install clang -y\n"+
+                "[ -d "+apiPackagePath+" ] || pkg upgrade -o DPkg::Options::=\"--force-confold\" -y\n"+
                 "[ -d "+apiPackagePath+" ] || git clone https://github.com/albbus-stack/snap4all-termux-api-package\n"+
                 "[ -f "+apiPackagePath+"/Makefile ] || cd snap4all-termux-api-package\n"+
                 "[ -f "+apiPackagePath+"/Makefile ] || cmake CMakeLists.txt\n"+
